daf2e2bfa6f4b8f7a15ce1eda214e57881ce8f40
daniel4w <daniel4w@gmx.de>
2016-08-09 09:26:51 +0200

    Added comments

--- classes/class.ilCourseSubscriptionMailPlugin.php
+++ classes/class.ilCourseSubscriptionMailPlugin.php
@@ -1,11 +1,27 @@
 <?php
-	include_once("./Services/EventHandling/classes/class.ilEventHookPlugin.php");
 
-	class ilCourseSubscriptionMailsPlugin() extends ilEventHookPlugin{
-		final function getPluginName(){
-			return "CourseSubscriptionMails";
-		}
-		private function init(){
-			
-		}
+include_once("./Services/EventHandling/classes/class.ilEventHookPlugin.php");
+
+/**
+ *  Listen on course subscriptors
+ */
+class ilCourseSubscriptionMailsPlugin() extends ilEventHookPlugin{
+	
+	/**
+	* Get Plugin Name. Must be same as in class name il<Name>Plugin
+	* and must correspond to plugins subdirectory name.
+	*
+	* @return	string	Plugin Name
+	*/
+	final function getPluginName(){
+		return "CourseSubscriptionMails";
 	}
+
+	/**
+	* Object initialization. Can be overwritten by plugin class
+	* (and should be made private final)
+	*/
+	private function init(){
+		
+	}
+}
